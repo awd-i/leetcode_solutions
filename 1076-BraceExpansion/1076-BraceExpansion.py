cf785fd6f434f73a029a2b80339ddc12bf0ada76
AWD <whitedeer@stanford.edu>
2025-11-11 01:55:41 -0800

    Create 1076-BraceExpansion

---
+++ 1076-BraceExpansion/1076-BraceExpansion.py
@@ -0,0 +1,21 @@
+# Last updated: 11/11/2025, 1:55:41 AM
+class Solution(object):
+    def expand(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        self.res = []
+        def helper(s, word):
+            if not s:
+                self.res.append(word)
+            else:
+                if s[0] == "{":
+                    i = s.find("}")
+                    for letter in s[1:i].split(','):
+                        helper(s[i+1:], word+letter)
+                else:
+                    helper(s[1:], word + s[0])
+        helper(S, "")
+        self.res.sort()
+        return self.res
