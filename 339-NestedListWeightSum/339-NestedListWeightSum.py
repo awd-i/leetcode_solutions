ba9e687cff94a61c37d144ee083051de0a98dcae
AWD <whitedeer@stanford.edu>
2025-11-11 01:56:55 -0800

    Create 339-NestedListWeightSum

---
+++ 339-NestedListWeightSum/339-NestedListWeightSum.py
@@ -0,0 +1,62 @@
+# Last updated: 11/11/2025, 1:56:54 AM
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        S = 0
+        def dfs(nested, depth):
+            if nested.isInteger():
+                return nested.getInteger() * depth
+            total = 0
+            for child in nested.getList():
+                total += dfs(child, depth + 1)
+            return total
+
+        S = 0
+        for item in nestedList:
+            S += dfs(item, 1)
+        return S
+                
+
+        
