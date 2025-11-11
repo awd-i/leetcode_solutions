4b355e1ef63a8620856d33d3090bda01d9934d9b
AWD <whitedeer@stanford.edu>
2025-11-11 01:57:01 -0800

    Create 277-FindTheCelebrity

---
+++ 277-FindTheCelebrity/277-FindTheCelebrity.py
@@ -0,0 +1,23 @@
+# Last updated: 11/11/2025, 1:57:00 AM
+# The knows API is already defined for you.
+# return a bool, whether a knows b
+# def knows(a: int, b: int) -> bool:
+
+class Solution:
+    def findCelebrity(self, n: int) -> int:
+
+        def is_celebrity(i):
+            for j in range(n):
+                if i == j: 
+                    continue
+                if knows(i, j) or not knows(j, i):
+                    return False
+            return True
+
+        candidate = 0
+        for i in range(1, n):
+            if knows(candidate, i):
+                candidate = i
+        if is_celebrity(candidate):
+            return candidate
+        return -1
