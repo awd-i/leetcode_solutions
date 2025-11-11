cae1cb4868dfd81ef49aba821052715f7f113e34
AWD <whitedeer@stanford.edu>
2025-11-11 01:56:52 -0800

    Create 359-LoggerRateLimiter

---
+++ 359-LoggerRateLimiter/359-LoggerRateLimiter.py
@@ -0,0 +1,21 @@
+# Last updated: 11/11/2025, 1:56:52 AM
+class Logger:
+
+    def __init__(self):
+        self.timestamps = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message in self.timestamps:
+            if timestamp >= self.timestamps[message]:
+                self.timestamps[message] = timestamp + 10
+                return True
+            else:
+                return False
+        else:
+            self.timestamps[message] = timestamp + 10
+            return True
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
