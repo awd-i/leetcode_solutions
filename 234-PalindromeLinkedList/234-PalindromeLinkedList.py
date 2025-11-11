6682bbbfaa28e79d902783b75dcbd45a95cb528c
AWD <whitedeer@stanford.edu>
2025-11-11 01:57:10 -0800

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.py
@@ -0,0 +1,18 @@
+# Last updated: 11/11/2025, 1:57:10 AM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        vals = []
+        curr = head
+        while curr:
+            vals.append(curr.val)
+            curr = curr.next
+        return vals == vals[::-1]
+    
+    
+
