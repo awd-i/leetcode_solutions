9fa0ed66fce73d3febca6bc73ea36f73d8e73865
AWD <whitedeer@stanford.edu>
2025-11-11 01:56:10 -0800

    Create 253-MeetingRoomsII

---
+++ 253-MeetingRoomsII/version/python/253-MeetingRoomsII_20251111_015609.py
@@ -0,0 +1,24 @@
+# Last updated: 11/11/2025, 1:56:09 AM
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        heap = []
+        intervals.sort(key = lambda x: x[0]) # sort by start time
+        heapq.heappush(heap, intervals[0][1]) # push first and its end time
+        for i in intervals[1:]: # check remaining
+            if heap[0] <= i[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, i[1]) # queue another room with a new end time
+        return len(heap)
+
+        
+
+
+        
+        
+            
+            
+
+        
